94e8d2e3847f2f23d22fe5519e956f06ab51eaa2
Jaeyoung Park <jaeyoungs.park@gmail.com>
2021-08-31 11:09:33 -0400

    Initial working commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # raii
+This RAII library utilize std::shared_ptr to manage its reference count. The
+objects are going to share their common pointer when it is copied automatically.
+
+
---
+++ include/raii/raii.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <memory>
+#include <functional>
+
+namespace raii {
+
+template <typename T> class AutoDeletable {
+private:
+  std::shared_ptr<T> _storage;
+
+public:
+  AutoDeletable() : _storage(nullptr) {}
+
+  AutoDeletable(AutoDeletable const &rhs) : _storage(rhs._storage) {}
+
+  AutoDeletable(AutoDeletable const &&rhs)
+      : _storage(std::move(rhs._storage)) {}
+
+  AutoDeletable(T handle, std::function<void(T)> deleter)
+      : _storage(std::shared_ptr<T>(new T(handle), [deleter](T *ptr) {
+	  deleter(*ptr);
+	  delete ptr;
+	})) {}
+
+  AutoDeletable<T> &operator=(const AutoDeletable<T> &rhs) {
+    this->_storage = rhs._storage;
+  }
+
+  AutoDeletable<T> &operator=(const AutoDeletable<T> &&rhs) {
+    this->_storage = rhs._storage;
+    rhs._storage = nullptr;
+  }
+
+  operator T() & { return _storage ? *_storage : std::declval<T>(); }
+
+  operator T() const & { return _storage ? *_storage : std::declval<T>(); }
+
+  // XXX: To prevent slicing, rvalue cant note be converted implicitly.
+  operator T() && = delete;
+
+  // XXX: To prevent slicing, rvalue cant note be converted implicitly.
+  operator T() const && = delete;
+
+  const T getVal() const { return _storage ? *_storage : std::declval<T>(); }
+};
+
+template <typename T>
+static auto MakeAutoDeletable(T handle, std::function<void(T)> deleter)
+    -> AutoDeletable<T> {
+  return AutoDeletable<T>(handle, deleter);
+}
+} // namespace raii
+

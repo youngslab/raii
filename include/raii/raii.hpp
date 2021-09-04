5805f261dd9d22effb16297504f829dea3f6ca74
Jaeyoung Park <jaeyoungs.park@gmail.com>
2021-09-04 10:12:32 -0400

    feat) New Dependency Feature

--- include/raii/raii.hpp
+++ include/raii/raii.hpp
@@ -5,45 +5,66 @@
 #include <any>
 #include <vector>
 
+#include <iostream>
+#include <fmt/format.h>
+
 namespace raii {
 
 template <typename T> class AutoDeletable {
 private:
-  std::shared_ptr<T> _storage;
-	std::vector<std::any> _deps;
+  class Storage {
+  public:
+    T value;
+    std::vector<std::any> deps;
+    Storage(T handle) : value(handle) {}
+  };
+
+  std::shared_ptr<Storage> _storage;
 
 public:
-  AutoDeletable() : _storage(nullptr) {}
-
-  AutoDeletable(AutoDeletable const &rhs) : _storage(rhs._storage) {}
-
-  AutoDeletable(AutoDeletable const &&rhs)
-      : _storage(std::move(rhs._storage)) {}
+  using inner_type_t = T;
 
   AutoDeletable(T handle, std::function<void(T)> deleter)
-      : _storage(std::shared_ptr<T>(new T(handle), [deleter](T *ptr) {
-	  deleter(*ptr);
+      : _storage(new Storage(handle), [deleter](Storage *ptr) {
+	  deleter(ptr->value);
+	  ptr->deps.clear();
 	  delete ptr;
-	})) {}
+	}) {}
+
+  AutoDeletable() : _storage(nullptr) {}
+
+  explicit AutoDeletable(AutoDeletable const &rhs) : _storage(rhs._storage) {}
+
+  explicit AutoDeletable(AutoDeletable &&rhs)
+      : _storage(std::move(rhs._storage)) {}
 
   AutoDeletable<T> &operator=(const AutoDeletable<T> &rhs) {
     this->_storage = rhs._storage;
+    return *this;
   }
 
-  AutoDeletable<T> &operator=(const AutoDeletable<T> &&rhs) {
-    this->_storage = rhs._storage;
-    rhs._storage = nullptr;
+  AutoDeletable<T> &operator=(AutoDeletable<T> &&rhs) {
+    this->_storage = std::move(rhs._storage);
+    return *this;
   }
 
-	~AutoDeletable() {
-		this->_storage = nullptr;
+  ~AutoDeletable() {}
 
-		this->_deps.clear();
-	}
+  operator T() & {
+    if (_storage == nullptr) {
+      throw std::runtime_error(
+	  fmt::format("Raii Not Initilized(&) this={}", fmt::ptr(this)));
+    }
+    return (*_storage).value;
+  }
 
-  operator T() & { return *_storage; }
-
-  operator T() const & { return *_storage ; }
+  operator T() const & {
+    if (_storage == nullptr) {
+      throw std::runtime_error(
+	  fmt::format("Raii Not Initilized(const&) this={}", fmt::ptr(this)));
+    }
+    return (*_storage).value;
+  }
 
   // XXX: To prevent slicing, rvalue cant note be converted implicitly.
   operator T() && = delete;
@@ -51,9 +72,13 @@ public:
   // XXX: To prevent slicing, rvalue cant note be converted implicitly.
   operator T() const && = delete;
 
-	auto dependOn(std::any dependency) -> void { _deps.push_back(dependency);}
+  auto dependOn(std::any dependency) -> void {
+    (*_storage).deps.push_back(dependency);
+  }
 
-  const T getVal() const { return *_storage ; }
+  const T getVal() const { return (*_storage).value; }
+
+  auto validate() -> bool { return _storage != nullptr; }
 };
 
 template <typename T>
